aed2f05a2792c323a1c25492a580ac7beb8e8675
Wes Higbee <wes.mcclure@gmail.com>
2025-02-16 17:52:56 -0600

    verbose again

--- qwen/dump_model_info.py
+++ qwen/dump_model_info.py
@@ -1,9 +1,9 @@
 from llama_cpp import Llama
 
 models_dir = "/home/wes/repos/github/ggerganov/llama.cpp/models"
-llm = Llama(model_path=models_dir + "/qwen2.5-coder-0.5b-instruct-q8_0.gguf", verbose=False)
+llm = Llama(model_path=models_dir + "/qwen2.5-coder-0.5b-instruct-q8_0.gguf", verbose=True)
+# verbose=True dumps model details
 # this is enough to dump model info to STDOUT... don't need to parse out specific values here
-# from pprint import pprint
-# pprint(llm)
-vocab_size = llm.n_vocab()
-print("Vocabulary size:", vocab_size)
+
+# vocab_size = llm.n_vocab()
+# print("Vocabulary size:", vocab_size)
